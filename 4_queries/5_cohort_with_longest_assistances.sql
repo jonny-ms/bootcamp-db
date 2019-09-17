286ae11dbb9cf37054e2cfb2ebd1f1bc45b338e1
Jonathan Milne-Skarzenski <j.milne21@gmail.com>
2019-09-17 01:51:34 +0000

    Add queries on assistance requests data

---
+++ 4_queries/5_cohort_with_longest_assistances.sql
@@ -0,0 +1,7 @@
+SELECT cohorts.name as cohort, avg(completed_at - started_at) as average_assistance_time
+FROM assistance_requests
+JOIN students ON students.id = assistance_requests.student_id
+JOIN cohorts ON cohorts.id = cohort_id
+GROUP BY cohort
+ORDER BY average_assistance_time DESC
+LIMIT 1;
