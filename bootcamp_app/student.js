0ab74249a5911b4834e957156acbe4082658f037
Jonathan Milne-Skarzenski <j.milne21@gmail.com>
2019-09-18 19:15:59 +0000

    Edit vulnerable queries to parameterized queries to protect from SQL injection

--- bootcamp_app/student.js
+++ bootcamp_app/student.js
@@ -7,13 +7,19 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
-SELECT students.id as student_id, students.name as name, cohorts.name as cohort
-FROM students
-JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
-LIMIT ${process.argv[3] || 5};
-`)
+const queryString = `
+  SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+  FROM students
+  JOIN cohorts ON cohorts.id = cohort_id
+  WHERE cohorts.name LIKE $1
+  LIMIT $2;
+  `;
+
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+const values = [`%${cohortName}%`, limit];
+
+pool.query(queryString, values)
 .then(res => {
   console.log(res.rows);
 })
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,14 +7,20 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
-SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
-FROM teachers
-JOIN assistance_requests ON teachers.id = teacher_id
-JOIN students ON students.id = assistance_requests.student_id
-JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
-ORDER BY teacher;`)
+const queryString = `
+  SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+  FROM teachers
+  JOIN assistance_requests ON teachers.id = teacher_id
+  JOIN students ON students.id = assistance_requests.student_id
+  JOIN cohorts ON cohorts.id = cohort_id
+  WHERE cohorts.name = $1
+  ORDER BY teacher;
+  `;
+
+const cohortName = process.argv[2] || 'JUL02';
+const values = [`${cohortName}`];
+
+pool.query(queryString, values)
 .then(res => {
   res.rows.forEach(row => {
     console.log(`${row.cohort}: ${row.teacher}`);
